afb6d9d379a70bd0ead49ad0f8048171c6895efa
olavvatne <olavvatne@gmail.com>
2014-06-15 14:34:57 +0200

    Used yeoman angular generator to generate a angular seed project. All files generated by angular-generator

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name canBeDoneApp
+ * @description
+ * # canBeDoneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('canBeDoneApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
