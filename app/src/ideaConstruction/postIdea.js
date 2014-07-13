e7b9f3f0ebeb1ecf5c5f19385821e2b3c556cc4c
olavvatne <olavvatne@gmail.com>
2014-07-13 13:10:56 +0200

    Fix to the idea form and sessionManager

--- app/src/ideaConstruction/postIdea.js
+++ app/src/ideaConstruction/postIdea.js
@@ -11,21 +11,14 @@ angular.module('cbdIdeaConstruction', ['cbdCommon'])
 .controller('postIdeaCtrl', ['$scope','ideaService',
 function ($scope,ideaService) {
 
-    $scope.isRestrictedSelected = false;
+    
     $scope.submitted = false;
     $scope.languages = [{name: 'English'}, {name: 'Norwegian'}];
     $scope.language = $scope.languages[0];
     $scope.idea = {};
     $scope.idea.accessOptions = {};
 
-    $scope.showOptionsForRestricted = function(show) {
-
-       $scope.isRestrictedSelected = show;
-        if(show == false) {
-            $scope.idea.accessOptions = {};
-
-        }
-    };
+    
 
     $scope.submit = function() {
 
@@ -33,7 +26,7 @@ function ($scope,ideaService) {
             .then(function(data) {
                 console.log("funket");
             }, function(error) {
-                console.log("funket ikke");
+                console.log(error);
             });
             
 
