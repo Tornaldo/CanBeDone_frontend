9f968b3c8571d0da37faa228021b83dbd97ca46e
olavvatne <olavvatne@gmail.com>
2014-08-31 17:11:12 +0200

    Category picker progress. Clicking main will retreive and display sub categories

---
+++ app/common/services/categoryService.js
@@ -0,0 +1,19 @@
+angular.module('cbdCommon').
+factory('categoryService', ['baseService', 'config',
+function (baseService, config) {
+
+    return {
+        getMainCategories: function() {
+        //wrong url. need api endpoint.
+        var url = config.apiBaseUrl + 'categories?mainCategoriesOnly=true';
+        return baseService.getResources(url);
+        },
+
+        getSubcategory: function(categoryId) {
+        //wrong url. Need api endpoint.
+        var url = config.apiBaseUrl + 'categories/'+ categoryId +'?numberOfLevels=2';
+        return baseService.getResources(url);
+        },
+    };
+
+}]);
--- app/src/ideaConstruction/categoryPicker/categoryPicker.js
+++ app/src/ideaConstruction/categoryPicker/categoryPicker.js
@@ -8,11 +8,32 @@ angular.module('cbdIdeaConstruction')
     },
 
     templateUrl: 'src/ideaConstruction/categoryPicker/category-picker.tpl.html',
-    controller: ['$scope', function($scope) {
-      
+    controller: ['$scope','categoryService', function($scope, categoryService) {
+      $scope.main = [];
+      $scope.sub = {};
+      $scope.getMain = function() {
+        categoryService.getMainCategories()
+        .then(function(data) {
+          $scope.main = data.categories;
+        });
+
+        $scope.getSubcategory = function(category, show) {
+          if(show) {
+            categoryService.getSubcategory(category.id)
+          .then(function(data) {
+            $scope.sub[category.id] = data;
+          });
+          }
+          else {
+            $scope.sub[category.id] = undefined;
+          }
+          
+        }
+      }
     }],
 
     link: function(scope, elem, attrs) {
+      scope.getMain();
     }
   };
 }]);
