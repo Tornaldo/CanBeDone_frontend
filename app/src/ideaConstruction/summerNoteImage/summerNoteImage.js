4f3e893db175905b818be24c507c10755b2e03d1
unknown <lordakarias@gmail.com>
2015-02-09 16:13:38 +0100

    images with captions

--- app/src/ideaConstruction/summerNoteImage/summerNoteImage.js
+++ app/src/ideaConstruction/summerNoteImage/summerNoteImage.js
@@ -1,4 +1,4 @@
-angular.module('cbdSummerNoteTest', [])
+angular.module('cbdSummerNoteTest', ['ngCkeditor'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/summerNoteImage', {
@@ -7,10 +7,15 @@ angular.module('cbdSummerNoteTest', [])
       })
   }])
 
+
+
 .controller('summernotetestCtrl', ['$scope','ideaService', 'notification', 'FileUploader', 'config', '$http',
 function ($scope,ideaService, notification, FileUploader, config, $http) {
     $scope.idea = {};
-    $scope.modelName;
+          $scope.editorOptions = {
+                language: 'ru'
+               // uiColor: '#000000'
+            };
           /*$scope.ideaDescriptionImage;
           $scope.clickidea = function(){
             console.log('$scope.title :' + $scope.idea.title);
@@ -18,64 +23,103 @@ function ($scope,ideaService, notification, FileUploader, config, $http) {
             console.log('$scope.shortDescriptionWhy :' + $scope.idea.shortDescriptionWhy);
             console.log('$scope.description :' + $scope.idea.description);
             console.log('$scope.ideaDescriptionImage :' + $scope.ideaDescriptionImage);      
-          }*/
+          
+CKEDITOR.on( 'pluginsLoaded', function() {
+  editor.widgets.on( 'instanceCreated', function( evt ) {
+    var widget = evt.data;
 
-         CKEDITOR.on('dialogDefinition', function(ev) {
-                // Take the dialog name and its definition from the event data
-                var dialogName = ev.data.name;
-                var dialogDefinition = ev.data.definition;
+    if ( widget.name != 'image' )
+      return;
 
-                if (dialogName == 'image') {
-                   dialogDefinition.onOk = function(e) {
-                    console.log("ok");
-                      var imageSrcUrl = e.sender.originalElement.$.src;
-                     // var imgHtml = CKEDITOR.dom.element.createFromHtml("<img src=" imageSrcUrl "/>");
-                      //CKEDITOR.instances.body.insertElement(imageSrcUrl);
-                                console.log('image upload:', imageSrcUrl);
-                  $scope.files = imageSrcUrl;  
-                  
-                 // console.log('idea : ' +$scope.ideamodel.description);
-                   };
+    widget.on( 'data', function() {
+      var alt = !!this.data.alt,
+        captionEditable = this.editables.caption;
+
+      // If alt is set and caption editable is already defined and its value equals
+      // the placeholder's value (the default one) then set the alt as the value.
+      if ( alt && captionEditable && ( captionEditable.getData() == editor.lang.image2.captionPlaceholder ) ) {
+        this.editables.caption.setData( alt );
+      }
+    } );
+  } );
+} );
+CKEDITOR.on( 'dialogDefinition', function( ev ) {
+    // Take the dialog name and its definition from the event data
+    var dialogName = ev.data.name,
+        dialogDefinition = ev.data.definition;
+
+    if ( dialogName == 'image') {
+        var onOk = dialogDefinition.onOk;
+        dialogDefinition.onOk = function( e ) {
+          var input = this.getContentElement( 'info', 'txtUrl' ),
+            imageSrcUrl = input.getValue();
+            $scope.idea.description = imageSrcUrl;
+            console.log(' idea:' + $scope.idea);      
+            console.log(' imageSrcUrl:' + imageSrcUrl);      
+            //! Manipulate imageSrcUrl and set it 
+            input.setValue( imageSrcUrl );
+            onOk && onOk.apply( this, e );  
+        };
+    }
+
+    if (dialogName == 'image2') {        
+            var infoTab = dialogDefinition.getContents('info');
+
+            infoTab.add({
+                type: 'text',
+                width: '50px',
+                label : 'Border Thickness',
+                id : 'borderField',
+                'default' : '0',
+                validate : function()
+                {
+                    if ( /[^\d]/.test( this.getValue() ) )
+                        return borderErrorMessage;
                 }
-          });
-
-         $scope.add = function(){
-            var url = config.apiBaseUrl + 'files';
-
-            var fd = new FormData()
-            angular.forEach($scope.files, function(file){
-                fd.append('file', file)
-            })
-
-            $http.post(url, fd, 
-            {
-                transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
-            }).success(function(data){
-                //Passing data to deferred's resolve function on successful completion
-                console.log(data)
-                console.log ('url: ' + url);
-                $scope.ideamodel.description = $scope.content + "+" + data.url;
-                   ideaService.postIdea($scope.ideamodel)
-                    .then(function(data) {
-                        notification.success("Idea posted");
-                    }, function(error) {
-                        notification.error("Could not post your idea." + error);                    
-                    });                 
-            }).error(function(error){
-                console.log(error)
-                //Sending a friendly error message in case of failure
             });
-        }
 
-    
-        CKEDITOR.replace( 'editor2', {
-            extraPlugins: 'image2',
-            height: 450
+            var dialog = dialogDefinition.dialog;
+            dialog.on('show', function () {
+                var image = this.widget.parts.image;
 
-        } );
-  
+                var borderField = this.getContentElement('info', 'borderField');
+
+                //Using jQuery, I can get the border of the image.
+                //You can use any other methods you prefer
+                var borderWidth = $(image.$).css("border-left-width").replace(/[^\d]+/g, '');
+                borderWidth = borderWidth ? borderWidth : 0;
+
+                borderField.setValue(borderWidth);
+            });
+            dialog.on('ok', function () {
+               var input = this.getContentElement( 'info', 'src' ),
+                imageSrcUrl = input.getValue();
+
+                var test = this.getContentElement( 'info', 'hasCaption' ),
+                caption = test.getValue();
+             //   var captionEditable = this.editables.caption;                
+              //  console.log("somehting here caption: " + captionEditable);
+              $scope.somefunt();
+                console.log("somehting here imgsrc: " + imageSrcUrl);
+                var image = this.widget.parts.image;
+                console.log("image : " + image);
+                var borderField = this.getContentElement('info', 'borderField');
+
+                var newBorderWidth = borderField.getValue() + 'px';
+
+                image.setStyle('border-style', 'solid');
+                image.setStyle('border-width', newBorderWidth);
+            });
+        }  
+});}*/
+
+
+
+
     }])
+
+
+
 
 .directive('summerEditor', [ function() {
   return {
@@ -85,7 +129,7 @@ function ($scope,ideaService, notification, FileUploader, config, $http) {
       ideamodel: '=',
     },
 
-    template: '<summernote config="options"  ng-model="content" id="ideaDescription"  on-image-upload="imageUpload(files, editor);" editable="editable"  '+
+    template: '<summernote config="options" on-change="change(contents)" ng-model="content" id="ideaDescription" on-image-upload="imageUpload(files, editor)"  '+
             '></summernote>'+
                  '<br>'+
             '<button ng-click="add()">Add</button>'+
@@ -96,8 +140,8 @@ function ($scope,ideaService, notification, FileUploader, config, $http) {
       
     $scope.ideamodel.categoryIds = [];
       $scope.options = {
-        height: 200,
-        width: 600,
+        height: 600,
+        width: 800,
         minHeight: null,
         maxHeight: null,
         toolbar: [
@@ -110,41 +154,107 @@ function ($scope,ideaService, notification, FileUploader, config, $http) {
           ['view', ['fullscreen', 'codeview']]
         ]
       };
-
+       $scope.imageUrls = [];
+       $scope.imageUrlscounter = 0;
+       $scope.imagCaption = [];
+       $scope.imageCaptionCounter = 0;
+       $scope.summerNoteContent = ''
         $scope.imageUpload = function(files, editor) {
           console.log('image upload:', files);
           $scope.files = files;  
-          
-          console.log('idea : ' +$scope.ideamodel.description);
+
+          console.log("$scope.imagCaption: " + $scope.imagCaption);
+
+         /* var file = files[0];
+          var reader = new FileReader();
+          reader.onload = function(readerEvt) {
+            var binaryString = readerEvt.target.result;
+            var bina= btoa(binaryString);
+            $( ".note-editable" ).append("<table class='table' style='text-align: center; width:30%;'><tbody><tr><td><figure><img src=data:"+files[0].type+";base64,"+bina+ " class="+Math.random()+" /></td></tr><tr><td><figcaption><input value="+"caption"+" style='text-align: center;' type="+"text"+" class="+"par"+" ng-model="+"captiondf"+"></figcaption></td><figure></tr></tbody></table>");
+
+              var bla= $('.caption-input').val();
+              $( "input.par" )
+                .keyup(function() {
+                  var value = $( this ).val();
+                  $scope.imagCaption = $( this ).val();
+                  $( "p.disp" ).text( value );
+                })
+                .keyup();
+          };
+          reader.readAsBinaryString(file);*/
+          $scope.getImageUrl();
+        }
+
+        $scope.change = function(contents) {
+          $scope.summerNoteContent = contents;
+          //console.log("contents : " + contents);
+          //console.log("$scope.imagCaption : " + $scope.imagCaption);
+        };
+
+        $scope.getImageUrl = function(){
+            var url = config.apiBaseUrl + 'files';
+            $scope.imagCaption[$scope.imageCaptionCounter] = "caption";
+            var fd = new FormData()
+            angular.forEach($scope.files, function(file){
+                fd.append('file', file)
+            })
+            console.log('url');
+            $http.post(url, fd, 
+            {
+              transformRequest: angular.identity,
+              headers: {'Content-Type': undefined}
+            }).success(function(data){
+                notification.success("Image posted");              
+                $scope.imageUrls[$scope.imageUrlscounter] = data.url;
+                $( ".note-editable" ).append("<table class='table' style='text-align: center; width:30%;'><tbody><tr><td><img src="+$scope.imageUrls[$scope.imageUrlscounter]+ " class="+Math.random()+" /></td></tr><tr><td><figcaption><span>Caption</span></figcaption></td></tr></tbody></table>");
+
+                $scope.imageUrlscounter += 1;
+                $scope.imageCaptionCounter +=1;
+                console.log("$scope.imageUrls: " + $scope.imageUrls); 
+                
+                console.log("$scope.imagCaption: " + $scope.imagCaption); 
+
+                            
+            }).error(function(error){
+              console.log(error)
+              //Sending a friendly error message in case of failure
+            });
         }
 
         $scope.add = function(){
-            var url = config.apiBaseUrl + 'files';
+          $scope.ideamodel.description = $scope.summerNoteContent;
+          ideaService.postIdea($scope.ideamodel)
+            .then(function(data) {
+              notification.success("Idea posted");
+           }, function(error) {
+              notification.error("Could not post your idea." + error);                    
+          });   
+           /* var url = config.apiBaseUrl + 'files';
 
             var fd = new FormData()
             angular.forEach($scope.files, function(file){
                 fd.append('file', file)
             })
-
+            console.log('url');
             $http.post(url, fd, 
             {
-                transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
+              transformRequest: angular.identity,
+              headers: {'Content-Type': undefined}
             }).success(function(data){
-                //Passing data to deferred's resolve function on successful completion
-                console.log(data)
-                console.log ('url: ' + url);
-                $scope.ideamodel.description = $scope.content + "+" + data.url;
-                   ideaService.postIdea($scope.ideamodel)
-                    .then(function(data) {
-                        notification.success("Idea posted");
-                    }, function(error) {
-                        notification.error("Could not post your idea." + error);                    
-                    });                 
+              //Passing data to deferred's resolve function on successful completion
+              console.log(data)
+              console.log ('url: ' + url);
+              $scope.ideamodel.description =  data.url+ "+" + $scope.imagCaption;
+              ideaService.postIdea($scope.ideamodel)
+              .then(function(data) {
+                notification.success("Idea posted");
+              }, function(error) {
+                notification.error("Could not post your idea." + error);                    
+              });                 
             }).error(function(error){
-                console.log(error)
-                //Sending a friendly error message in case of failure
-            });
+              console.log(error)
+              //Sending a friendly error message in case of failure
+            });*/
         }
 
     }],
@@ -156,9 +266,9 @@ function ($scope,ideaService, notification, FileUploader, config, $http) {
 
 
 
-/*.directive('fileInput', ['$parse', function($parse){
+.directive('fileInput', ['$parse', function($parse){
     return {
-        restrict: 'A', 
+        restrict: 'AE', 
         link: function(scope,elm,attrs) {
             elm.bind('change', function(){
                $parse(attrs.fileInput) 
@@ -167,7 +277,7 @@ function ($scope,ideaService, notification, FileUploader, config, $http) {
             })
         }
     }
-}])*/
+}])
 
 
 
