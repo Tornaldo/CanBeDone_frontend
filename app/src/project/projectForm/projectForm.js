49ab32ef274ecad7bf8f6bc898b925c175826f38
Olav Vatne <olavvatne@gmail.com>
2015-03-01 15:19:07 +0100

    Form controller skeleton

--- app/src/project/projectForm/projectForm.js
+++ app/src/project/projectForm/projectForm.js
@@ -4,3 +4,25 @@ function ($scope, ideaId) {
     $scope.idea = {};
     $scope.idea.id = ideaId;
 }])
+
+.controller('ProjectFormCtrl', ['$scope', 'ideaService', 'notification', '$location',
+function ($scope, ideaService, notification, $location) {
+    $scope.faq = [{question: "", answer: ""}];
+    $scope.ideaId = $scope.$parent.idea.id;
+    //TODO: Make template and controller into a directive.
+    
+    $scope.submitProject = function() {
+        /*ideaService.postFaq($scope.ideaId, $scope.faq)
+        .then(function(success) {
+            notification.success("FAQ posted");
+            $location.path('/idea/' + $scope.ideaId  + '/');
+            if($scope.$parent.faq) {
+                $scope.$parent.faq.push.apply($scope.$parent.faq, $scope.faq)
+            }
+            $scope.faq = [{question: "", answer: ""}];
+        }, function(error) {
+            console.log(error);
+        });*/
+    };
+
+}])
