8fba514879ed3e0a795f80cfadea721fe594a7fe
Olav Vatne <olavvatne@gmail.com>
2015-03-01 14:41:05 +0100

    Embedded faq form in idea page. Refactor faq form controller and template so faq can both be added on seperate page or as an embedded form on the idea page

--- app/src/faq/faqForm/faqForm.js
+++ app/src/faq/faqForm/faqForm.js
@@ -1,16 +1,22 @@
+
 angular.module('cbdFaq' )
-.controller('FaqFormCtrl', ['$scope', 'ideaService', '$routeParams', 'ideaId', 'notification', '$location',
-function ($scope, ideaService, $routeParams, ideaId, notification, $location) {
+.controller('FaqFormCtrl', ['$scope', 'ideaService', 'notification', '$location',
+function ($scope, ideaService, notification, $location) {
     $scope.faq = [{question: "", answer: ""}];
-    $scope.ideaId = ideaId;
+    $scope.ideaId = $scope.$parent.idea.id;
+    //TODO: Make template and controller into a directive.
     
     $scope.submitFaq = function() {
         ideaService.postFaq($scope.ideaId, $scope.faq)
         .then(function(success) {
-        	notification.success("FAQ posted");
+            notification.success("FAQ posted");
             $location.path('/idea/' + $scope.ideaId  + '/');
+            if($scope.$parent.faq) {
+                $scope.$parent.faq.push.apply($scope.$parent.faq, $scope.faq)
+            }
+            $scope.faq = [{question: "", answer: ""}];
         }, function(error) {
-        	console.log(error);
+            console.log(error);
         });
     };
 
@@ -19,6 +25,12 @@ function ($scope, ideaService, $routeParams, ideaId, notification, $location) {
     }
 
     $scope.removeQuestionAnswer = function(idx) {
-    	$scope.faq.splice(idx, 1);
+        $scope.faq.splice(idx, 1);
     }
+}])
+
+.controller('FaqFormPageCtrl', ['$scope', 'ideaId', 'notification',
+function ($scope, ideaId, notification) {
+    $scope.idea = {};
+    $scope.idea.id = ideaId;
 }])
