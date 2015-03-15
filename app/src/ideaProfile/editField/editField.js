574f02a89d8847529d091d32d9600318a1877e90
Olav Vatne <olavvatne@gmail.com>
2015-03-15 16:13:40 +0100

    Refactors edit directives to one directive. One directive where user can chose template to use. Dynamic template possible because functionality identical for text title and field

--- app/src/ideaProfile/editField/editField.js
+++ app/src/ideaProfile/editField/editField.js
@@ -1,40 +1,6 @@
 'use strict';
 //TODO: Access element inside using transclude, for maximum reusablitiy
 angular.module('cbdIdea')
-.directive('editTitle', [ function() {
-  return {
-    restrict: 'AE',
-
-    scope: {
-      content: '=',
-      onEdit: '&',
-    },
-
-    template: 
-                '<h1 ng-show="!editorOn" class="idea-profile-title">'+
-                    '<b>{{content}}</b> <a class="btn-idea-editor" ng-click="editorOn=true"> <i class="fa fa-pencil-square-o"></i> </a>'+
-                '</h1>'+
-                '<h1 ng-show="editorOn" class="idea-profile-title">' +
-                    '<input ng-model="content" class="form-control form-control-edit">' +
-                    '<a  class="btn-idea-editor" ng-click="onEdit()"><i class="fa fa-floppy-o"></i></a> <span class="btn-idea-editor"> or</span>' +
-                    '<a  class="btn-idea-editor" ng-click="cancel()"><i class="fa fa-times"></i></a>' +
-                '</h1>'
-            ,
-    controller: ['$scope', function($scope) {
-      $scope.editorOn = false;
-      $scope.contentCopy = $scope.content;
-
-      $scope.cancel = function() {
-        $scope.editorOn = false;
-        $scope.content = $scope.contentCopy;
-      };
-    }],
-
-    link: function(scope, elem, attrs) {
-    }
-  };
-}])
-
 .directive('editField', [ function() {
   return {
     restrict: 'AE',
@@ -42,30 +8,19 @@ angular.module('cbdIdea')
     scope: {
       content: '=',
       onEdit: '&',
-      label: '@'
+      template: '@'
     },
 
-    template: 
-                '<div ng-show="!editorOn">' +
-                    '<p class="idea-profile-what-why-heading">{{label}}:</p>'+ 
-                    '<p><i><b>{{content}}</b></i>'+
-                   '<a class="btn-idea-editor" ng-click="editorOn=true"><i class="fa fa-pencil-square-o"></i></a></p>'+
-
-                '</div>'+
-                '<div ng-show="editorOn">'+
-                    '<p class="idea-profile-what-why-heading">{{label}}:</p> '+
-                    '<p>'+
-                        '<textarea ng-model="content" type="text" class="form-control form-control-edit" id="{{label}}" name="{{label}}" > </textarea>'+  
-                        '<p>'+
-                            '<a  ng-click="onEdit()"><i class="fa fa-floppy-o"></i></a> or'+
-                            '<a  ng-click="cancel()"><i class="fa fa-times"></i></a>'+
-                        '</p>'+
-                    '</p>'+
-                '</div>'
-            ,
+    template: '<div ng-include="selectedTemplate"></div>',
     controller: ['$scope', function($scope) {
       $scope.editorOn = false;
       $scope.contentCopy = $scope.content;
+
+       $scope.save = function() {
+        console.log("SAVE");
+        $scope.editorOn = false;
+        $scope.onEdit();
+      }
 
       $scope.cancel = function() {
         $scope.editorOn = false;
@@ -74,45 +29,17 @@ angular.module('cbdIdea')
     }],
 
     link: function(scope, elem, attrs) {
-    }
-  };
-}])
+      console.log("TYPE:" + scope.template);
+      if(scope.template == 'title') {
+        scope.selectedTemplate = 'src/ideaProfile/editField/edit-title.tpl.html';
+      }
+      else if(scope.template == 'text') {
+        scope.selectedTemplate = 'src/ideaProfile/editField/edit-text.tpl.html';
 
-.directive('editText', [ function() {
-  return {
-    restrict: 'AE',
-
-    scope: {
-      content: '=',
-      onEdit: '&',
-    },
-
-    template: 
-                '<div ng-show="!editorOn" class="idea-profile-description">'+
-                '<div id="description" ng-bind-html="content"></div> <a class="btn-idea-editor" ng-click="editorOn=true"> <i class="fa fa-pencil-square-o"></i> </a>'+
-            '</div>'+
-            '<div ng-show="editorOn" class="idea-profile-description">'+
-                '<p>'+
-                    '<textarea ng-model="content" type="text" class="form-control form-control-edit" id="what" name="what" > </textarea>'+  
-                    '<p>'+
-                        '<a  ng-click="onEdit()"><i class="fa fa-floppy-o"></i></a> or'+
-                        '<a  ng-click="cancel()"><i class="fa fa-times"></i></a>'+
-                    '</p>'+
-                '</p>'+
-            '</div>'
-            ,
-    controller: ['$scope', function($scope) {
-      $scope.editorOn = false;
-      $scope.contentCopy = $scope.content;
-
-      $scope.cancel = function() {
-        $scope.editorOn = false;
-        $scope.content = $scope.contentCopy;
-      };
-    }],
-
-    link: function(scope, elem, attrs) {
-      s
+      }
+      else {
+        scope.selectedTemplate = 'src/ideaProfile/editField/edit-field.tpl.html';
+      }
     }
   };
 }]);
